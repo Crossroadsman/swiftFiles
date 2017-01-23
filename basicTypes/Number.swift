c431129ffa7dd112695545db82324c88923c4d07
Alex Koumparos <crossroadsman@gmail.com>
2017-01-22 19:22:38 -0700

    Create Number.swift

---
+++ basicTypes/Number.swift
@@ -0,0 +1,32 @@
+/*
+
+This creates a general Number (Num) type like in Haskell
+
+*/
+
+import Foundation
+
+protocol Num {
+
+    // methods already implemented in number types in Swift
+    static func +(lhs: Self, rhs: Self) -> Self
+    static func -(lhs: Self, rhs: Self) -> Self
+    static func /(lhs: Self, rhs: Self) -> Self
+    static func *(lhs: Self, rhs: Self) -> Self
+    
+    /*
+    // methods in Haskell not implemented in every number type
+    // these would need to be implemented for each number type that wanted to conform to Num
+    static func negate(_ a: Self) -> Self
+    static func abs(_ a: Self) -> Self
+    static func signum(_ a: Self) -> Self
+    // not clear how the following could theoretically be implemented as Swift has no arbitrary size integer type
+    // static func fromInteger(_ a: Integer) -> Self
+    
+}
+
+extension Int: Num {}
+extension Double: Num {}
+extension Float: Num {}
+//etc
+
