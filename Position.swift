807cfb05fd2b7417950188ab1c1c04d049bf5294
Alex Koumparos <crossroadsman@gmail.com>
2017-10-08 00:35:20 -0600

    Update Position.swift

--- Position.swift
+++ Position.swift
@@ -1,8 +1,13 @@
 import Foundation
 
-struct Position {
+struct Position: Equatable {
     
-    private enum Constants {
+    //MARK: - Supporting Types
+    //------------------------
+    
+    //MARK: Constants
+    private enum PositionConstants {
+        
         static let min_x = 0
         static let max_x = 7
         
@@ -13,15 +18,28 @@ struct Position {
         static let max_z = 0
     }
     
+    //MARK: Other enums
+    private enum Axis {
+        case x, y, z
+    }
+    
+    private enum PositionError: Error {
+        case outOfBounds(axis: String, minValue: Int, maxValue: Int)
+    }
+    
+    //MARK: - Properties
+    //------------------
     private var x: Int
     private var y: Int
     private var z: Int
     
+    //MARK: - Initialisers
+    //--------------------
     init?(x: Int, y: Int, z: Int = 0) {
         
-        guard x >= min_x && x <= max_x &&
-              y >= min_y && y <= max_y &&
-            z >= min_z && z <= max_z else {
+        guard Position.isValid(axis: .x, value: x) &&
+              Position.isValid(axis: .y, value: y) &&
+              Position.isValid(axis: .z, value: z) else {
                 return nil
         }
         
@@ -30,4 +48,93 @@ struct Position {
         self.z = z
     }
     
+    //MARK: Accessors
+    //---------------
+    
+    public func getPosition() -> (x: Int, y: Int, z: Int) {
+        return (x, y, z)
+    }
+    
+    public mutating func setPosition(x: Int, y: Int, z: Int) throws {
+        guard Position.isValid(axis: .x, value: x) else {
+            throw PositionError.outOfBounds(axis: "x", minValue: PositionConstants.min_x, maxValue: PositionConstants.max_x)
+        }
+        guard Position.isValid(axis: .y, value: y) else {
+            throw PositionError.outOfBounds(axis: "y", minValue: PositionConstants.min_y, maxValue: PositionConstants.max_y)
+        }
+        guard Position.isValid(axis: .z, value: z) else {
+            throw PositionError.outOfBounds(axis: "z", minValue: PositionConstants.min_z, maxValue: PositionConstants.max_z)
+        }
+        
+        self.x = x
+        self.y = y
+        self.z = z
+    }
+    
+    //MARK: - Other Methods
+    //---------------------
+    /**
+     Checks whether a specified value is between the min and max for a particular axis.
+     */
+    private static func isValid(axis: Axis, value: Int) -> Bool {
+        switch axis {
+        case .x:
+            if value >= PositionConstants.min_x && value <= PositionConstants.max_x {
+                return true
+            }
+        case .y:
+            if value >= PositionConstants.min_y && value <= PositionConstants.max_y {
+                return true
+            }
+        case .z:
+            if value >= PositionConstants.min_z && value <= PositionConstants.max_z {
+                return true
+            }
+        }
+        return false
+        
+    }
+    
+    //MARK: - Protocol Conformance
+    //----------------------------
+    
+    //MARK: Equatable
+    static func ==(lhs: Position, rhs: Position) -> Bool {
+        if lhs.x == rhs.x &&
+           lhs.y == rhs.y &&
+           lhs.z == rhs.z {
+            return true
+        } else {
+            return false
+        }
+    }
 }
+
+// EXAMPLES
+var position1 = Position(x: -1, y: 8) // nil
+var position2 = Position(x: 3, y: 2, z: 0)
+let position3 = Position(x: 3, y: 2)
+let position4 = Position(x: 2, y: 3)
+let position5 = Position(x: 3, y: 2, z: 1)
+
+position2 == position3
+position3 == position4
+
+// Note that attempting to set the position of a nil Position will leave the entity as nil and won't raise an error
+position1?.getPosition()
+do {
+    try position1?.setPosition(x: 3, y: 2, z: 0)
+} catch {
+    print(error.localizedDescription)
+}
+position1?.getPosition()
+
+position2?.getPosition()
+do {
+    try position2?.setPosition(x: 4, y: 3, z: 0)
+} catch {
+    print(error.localizedDescription)
+}
+position2?.getPosition()
+
+position2?.getPosition().x
